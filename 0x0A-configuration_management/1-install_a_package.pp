797697f1d56b7f4f4669dfbe273fcfab913af68d
Ssekyene <robertssekyene05@gmail.com>
2024-07-15 08:58:12 -0700

    modifed file 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
 # Install flask v: 2.1.0 from pip3
+# ensure that werkzeug version is less than 3.* for
+# compatibility, for my case werkzeug v: 2.1.1
+# you can use `pip3 show` to check for versions
 
 package { 'flask':
 ensure   => '2.1.0',
-provider => 'pip'
+provider => 'pip3'
 }
