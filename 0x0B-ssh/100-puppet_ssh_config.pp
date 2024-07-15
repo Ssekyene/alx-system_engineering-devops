52dd9b95513a93ba8edbffce35fc774ae24af2dc
Ssekyene Robert <robertssekyene05@gmail.com>
2024-07-15 07:55:18 -0700

    fixed puppet lint

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # using Puppet to make changes to our configuration file  configured to refuse to authenticate using a password
 file_line { 'Turn off passwd auth':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config'
-  line    => 'PasswordAuthentication no';
+  ensure => present,
+  path   => '/etc/ssh/ssh_config'
+  line   => 'PasswordAuthentication no';
 }
 file_line{ 'Declare identity file':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config'
-  line    => 'IdentityFile ~/.ssh/school'
+  ensure => present,
+  path   => '/etc/ssh/ssh_config'
+  line   => 'IdentityFile ~/.ssh/school'
 }
