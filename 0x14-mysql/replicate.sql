da92edbec08bd3fc3b7f8e026f1228e403287a00
Ssekyene Robert <robertssekyene05@gmail.com>
2024-08-07 14:35:54 +0000

    added replication setup

---
+++ 0x14-mysql/replicate.sql
@@ -0,0 +1,13 @@
+-- setting up and starting replication
+STOP SLAVE;
+
+CHANGE MASTER TO
+MASTER_HOST='100.26.231.29',
+MASTER_USER='replica_user',
+MASTER_PASSWORD='replica_user',
+MASTER_LOG_FILE='mysql-bin.000001',
+MASTER_LOG_POS=154;
+
+START SLAVE;
+SHOW SLAVE STATUS\G
+
